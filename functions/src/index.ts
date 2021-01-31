3e57ab603a1e37605a2545e2b59162f379854ac1
Jay Hackett <codingjock@codingjock.com>
2021-01-31 06:05:16 -0700

    adds join mailing list and storage of orders

---
+++ functions/src/index.ts
@@ -0,0 +1,85 @@
+import * as functions from "firebase-functions";
+import { uid } from "uid";
+import * as sgMail from "@sendgrid/mail";
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const joinMailingList = functions.https.onRequest(async (request, response) => {
+  response.set("Access-Control-Allow-Origin", "*"); // you can also whitelist a specific domain like "http://127.0.0.1:4000"
+  response.set("Access-Control-Allow-Headers", "Content-Type");
+  const db = admin.firestore();
+  await db.collection('users').doc(request.body["email"]).set({
+    "email": request.body["email"],
+    "emailOptIn": true,
+    "emailOptInDateTime": new Date().toUTCString()
+  }, { merge: true });
+});
+export const createOrder = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.set("Access-Control-Allow-Origin", "*"); // you can also whitelist a specific domain like "http://127.0.0.1:4000"
+  response.set("Access-Control-Allow-Headers", "Content-Type");
+  try {
+    let orderId: string = uid(8);
+    var body = request.body;
+    // request.body["order_number"] = orderId;
+    console.log(request.body);
+    sgMail.setApiKey("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");
+
+    let orderData = {
+      "first_name": body["first_name"],
+      "last_name": body["last_name"],
+      "email": body["email"],
+      "address_line_1": body["address_line_1"],
+      "address_line_2": body["address_line_2"],
+      "city": body["city"],
+      "state_province_region": body["state_province_region"],
+      "postal_code": body["postal_code"],
+      "country": body["country"],
+      "phone_number": body["phone_number"],
+      "Sender_Name": "Hunters",
+      "Sender_Email": "sales@huntershempmt.com",
+      "Sender_Address": "",
+      "Sender_City": "Manhattan",
+      "Sender_State": "MT",
+      "Sender_Zip": 59741,
+      "Sender_Country": "United States",
+      "order_number": orderId,
+      "order_total": body["order_total"],
+      "huntersHempVenmoAccount": "@HuntersH-2019"
+    };
+    const msg = {
+      to: [body["email"], "sales@huntershempmt.com"], // Change to your recipient
+      from: "sales@huntershempmt.com", // Change to your verified sender
+      templateId: "d-c79c085b1a904832a28df63886c47347",
+      dynamicTemplateData: orderData,
+    };
+    console.log(msg);
+
+    sgMail
+      .send(msg)
+      .then(async () => {
+        console.log("Email sent");
+        const db = admin.firestore();
+
+        const res = await db.collection("orders").doc(orderData.order_number).set(orderData);
+        console.log(res);
+        response.send(orderData);
+      })
+      .catch((error) => {
+        console.error(error);
+        response.status(400).send("There was an error processing the request");
+      });
+  } catch (error) {
+    console.log(error);
+    response.status(400).send({
+      errorText: "An error occured while trying to process your request"
+    });
+
+  }
+
+});
